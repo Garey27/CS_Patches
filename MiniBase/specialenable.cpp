5bf0034abff4d1cdfde463b5f4ee5d576ee0bd23
wopox1337 <wopox1337@ya.ru>
2019-01-28 05:56:03 +0300

    change command for unblock in hw.dll

--- MiniBase/specialenable.cpp
+++ MiniBase/specialenable.cpp
@@ -6,10 +6,10 @@ void SpecialEnable()
 {
 	// add '_special' support for 48 protocol
 //	if(protocol_48)
-		g_pEngine->pfnAddCommand("special", CMDHandler_Special);
+		g_pEngine->pfnAddCommand("zpecial", CMDHandler_Special);
 }
 
 void CMDHandler_Special()
 {
-	g_pEngine->pfnClientCmd("_special");
+	g_pEngine->pfnClientCmd("_zpecial");
 }
